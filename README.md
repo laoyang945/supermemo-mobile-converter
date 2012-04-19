5a7c67f3e989e0d9b62f91153aef620e98293631
laoyang945 <laoyang945@gmail.com>
2012-04-19 18:02:16 +0800

    Option: choose normally or randomly generate QA file

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Motivation
 This python script is used to generate supermemo course from tab delimited text files.
 It can convert the source file to a QA format, which can be used in the supermemo on iPhone/iPad/iPod Touch.  
+You can choose randomly or normally generate the QA file from your source.
 
 # Usage
 - Make sure that your source text is encoded in UTF-8 and tab delimited.
-- [IMPORTANT] there should be one tab before the end of each line
 - After install python (2.7+), run the script directly
 - The phonetic symbols should be of the font "Ksphonet" by Kingsoft
 
--- sm-mobile-converter.py
+++ sm-mobile-converter.py
@@ -8,6 +8,7 @@ import sys
 import re
 import codecs
 import string
+import random
 QUESTION_FONT= 'Palatino-Bold'
 QUESTION_SIZE= '8'
 PHONETIC_SYMBOL_FONT= 'Palatino-Bold'
@@ -30,24 +31,43 @@ def generate_answer(answerText):
 	newstring=['&#716;','&#716;','&#712;','&#230;', '&#593;','&#596;','&#601;','&#643;', '&#618;','&#650;','&#331;','&#652;','&#596;','&#240;','u','&#658;','&#952;','&#604;','&#609;']
 	for k in range(0,len(oldstring)-1):
 		answerText=answerText.replace(oldstring[k],newstring[k])
-	return 'A: <font face="'+ ANSWER_FONT +'" size="'+ ANSWER_SIZE +'">'+answerText +'</font>'+'\n\n' 
+	return 'A: <font face="'+ ANSWER_FONT +'" size="'+ ANSWER_SIZE +'">'+answerText.strip() +'</font>'+'\n\n' 
 
 def main():
 	word_file_path=raw_input('input the path of the word list,the file should be encoded in utf-8 and delimited by Tab:')
 	word_file=codecs.open(word_file_path,'r','utf-8')
-	output_file=codecs.open('sm-iphone-'+time.strftime('%Y-%m-%d-%H-%M-%S')+ '.txt','w','utf-8')
+	output_file=codecs.open('sm-iOS-'+word_file_path,'w','utf-8')
 	questionIndex=raw_input('which column is the question:')
 	phoneticIndex=raw_input('which column is the phonetic symbol:')
 	answerIndex=raw_input('which column is the answer:')
-	while True:
-		line=word_file.readline()
-		if not line: break
-		if line.strip()=="": continue
-		questionText=line.split('\t')[string.atoi(questionIndex)-1]
-		phoneticText=line.split('\t')[string.atoi(phoneticIndex)-1]
-		answerText=line.split('\t')[string.atoi(answerIndex)-1]
-		out_text=generate_question(questionText.encode('utf-8'))+generate_phonetic_symbol(phoneticText.encode('utf-8'))+generate_answer(answerText.encode('utf-8'))
-		output_file.write(out_text.decode('utf-8'))
+	randomFlag=raw_input('randomly generate?[y/n]:')
+	if randomFlag=='n':
+		for line in word_file:
+			if line.strip()=="": continue
+			questionText=line.split('\t')[string.atoi(questionIndex)-1]
+			phoneticText=line.split('\t')[string.atoi(phoneticIndex)-1]
+			answerText=line.split('\t')[string.atoi(answerIndex)-1]
+			out_text=generate_question(questionText.encode('utf-8'))+generate_phonetic_symbol(phoneticText.encode('utf-8'))+generate_answer(answerText.encode('utf-8'))
+			output_file.write(out_text.decode('utf-8'))
+	else:
+		lines=[]
+		rawlines=word_file.readlines()
+		cnt=len(rawlines)
+		while(cnt>0):
+			try:
+				pos=random.randrange(0,cnt-1)
+			except:
+				pos=0
+			lines.append(rawlines[pos])
+			del rawlines[pos]
+			cnt=len(rawlines)
+		for line in lines:
+			if line.strip()=="": continue
+			questionText=line.split('\t')[string.atoi(questionIndex)-1]
+			phoneticText=line.split('\t')[string.atoi(phoneticIndex)-1]
+			answerText=line.split('\t')[string.atoi(answerIndex)-1]
+			out_text=generate_question(questionText.encode('utf-8'))+generate_phonetic_symbol(phoneticText.encode('utf-8'))+generate_answer(answerText.encode('utf-8'))
+			output_file.write(out_text.decode('utf-8'))
 	raw_input('Done')
 	word_file.close()
 	output_file.close()
